8b4c9628cbb71f490601a811c4514c38602317e4
ElyseeMontissol <Elysee.Montissol@gmail.com>
2024-08-31 19:39:44 -0400

    Initial commit with project files

---
+++ src/pages/StarterPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Index(props) {
+
+    document.title = "VetConnect"
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    );
+}
+
+export default Index;
